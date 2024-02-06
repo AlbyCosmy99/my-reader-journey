dd696a6c79ab99dd7d6c558bf276c347e8f83149
Cosmin Andrei Albu <dinamo1999@icloud.com>
2024-02-06 14:35:54 +0100

    changes

--- src/components/Intro/IsLogged/BooksList/BooksList.jsx
+++ src/components/Intro/IsLogged/BooksList/BooksList.jsx
@@ -1,13 +1,45 @@
+import { Container, Row, Col } from "react-bootstrap";
 import AddNewBookButton from "../../../Buttons/AddNewBookButton/AddNewBookButton";
 
 export default function BooksList({message}) {
+    const books = [5,3,5,2,6,3,3,5,1,6,8,5,3,4,6,6,6,6,5,43,32,3]
     return (
         <div className="books-list-container">
             <div className="add-new-book-btn">
                 <AddNewBookButton />
             </div>
-            <div>
-                ciao2
+            <div style={{marginLeft: '10rem', marginRight:'10rem'}}>
+                <div className="card" style={{maxHeight: '65vh', overflowY: 'auto'}}>
+                    <div className="card-header" style={{position: 'sticky', top: 0, zIndex: 1, backgroundColor: 'white', textAlign:'center'}}>
+                        ALL MY BOOKS
+                    </div>
+                    <div className="card-body">
+                        {books.map((book, index) => {
+                            return (
+                                <div className="card" key={index}>
+                                    <div className="card-body">
+                                        <Container>
+                                            <Row>
+                                                <Col lg={3} className="book-details" style={{textAlign:'center'}}>
+                                                    <img style={{ maxWidth: '100%', maxHeight: '150px' }} className="book-img img-fluid" src="https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg" alt="book cover"/>
+                                                </Col>
+                                                <Col lg={3} className="book-details">
+                                                    <h2>TITOLO</h2>
+                                                    <h4>AUTORE</h4>
+                                                </Col>
+                                                <Col lg={6}>
+                                                    <div style={{textAlign:'end', paddingRight:'3rem'}} className="d-flex justify-content-end">
+                                                        <i class="bi bi-heart" style={{ fontSize: '60px' }}></i>
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </Container>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     )
