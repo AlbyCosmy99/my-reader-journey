fef6ddaab79b273d429e1bff1c12156c47c95dc4
Cosmin Andrei Albu <dinamo1999@icloud.com>
2024-02-09 19:21:50 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Login from './components/Intro/Login/Login'
@@ -6,6 +6,15 @@ import Home from './components/Intro/IsLogged/Home/Home'
 
 function App() {
   const [isLogged, setIsLogged] = useState(false)
+
+  useEffect(() => {
+    if(localStorage.getItem('jwt')) {
+      setIsLogged(true)
+    }
+    else {
+      setIsLogged(false)
+    }
+  }, [])
   return (
     <div id='app-container'>
       <div id='content-container'>
