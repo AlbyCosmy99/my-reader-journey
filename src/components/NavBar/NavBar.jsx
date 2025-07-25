47cfaac6443b361e78a86360016ee6c64769ff02
Andrei Albu <dinamo1999@icloud.com>
2025-07-25 21:30:45 +0200

    feat

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Dropdown from 'react-bootstrap/Dropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './NavBar.css';
-import { useNavigate, Link } from 'react-router-dom';
-import consts from '../../consts';
+import { useState, useEffect, useRef } from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./NavBar.css";
+import { useNavigate, Link } from "react-router-dom";
+import consts from "../../consts";
 
 function NavBar() {
   const navigate = useNavigate();
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [showSearchDropdown, setShowSearchDropdown] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const [books, setBooks] = useState([]);
 
+  const dropdownRef = useRef(null);
+
   const handleLogout = () => {
-    localStorage.removeItem('jwt');
+    localStorage.removeItem("jwt");
     window.location.href = `${consts.getFrontendUrl()}`;
   };
 
   const handleSearchChange = (event) => {
     const value = event.target.value;
     fetch(`${consts.getBackendUrl()}/api/users/books?take=5`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("jwt")}`,
       },
     })
       .then((res) => res.json())
@@ -44,16 +46,29 @@ function NavBar() {
   };
 
   const navigatetoBook = (bookId) => {
-    setSearchValue('');
+    setSearchValue("");
     navigate(`/home/book/${bookId}`);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary custom-navbar-color">
       <Container fluid>
         <Navbar.Brand
           as={Link}
-          to={consts.getFrontendUrl() + '/home'}
+          to={consts.getFrontendUrl() + "/home"}
           className="logo text-light"
         >
           <span className="logo-reader">My</span>Reader
@@ -62,7 +77,11 @@ function NavBar() {
 
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll />
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          />
 
           <Form className="d-flex position-relative">
             <Form.Control
@@ -73,10 +92,10 @@ function NavBar() {
               value={searchValue}
               onChange={handleSearchChange}
             />
-            {showSearchDropdown && books.length > 0 && searchValue !== '' && (
+            {showSearchDropdown && books.length > 0 && searchValue !== "" && (
               <Dropdown.Menu
                 show={true}
-                style={{ position: 'absolute', left: 0, top: '100%' }}
+                style={{ position: "absolute", left: 0, top: "100%" }}
               >
                 {books.map((book, index) => (
                   <Dropdown.Item
@@ -88,18 +107,28 @@ function NavBar() {
                 ))}
               </Dropdown.Menu>
             )}
-            <Dropdown
-              align="end"
-              show={showDropdown}
-              onClick={() => setShowDropdown(!showDropdown)}
-            >
-              <Dropdown.Toggle variant="link" id="dropdown-basic" style={{ color: 'white' }}>
-                <i className="bi bi-person-circle" style={{ margin: '1rem', cursor: 'pointer' }}></i>
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+
+            <div ref={dropdownRef}>
+              <Dropdown
+                align="end"
+                show={showDropdown}
+                onClick={() => setShowDropdown(!showDropdown)}
+              >
+                <Dropdown.Toggle
+                  variant="link"
+                  id="dropdown-basic"
+                  style={{ color: "white" }}
+                >
+                  <i
+                    className="bi bi-person-circle"
+                    style={{ margin: "1rem", cursor: "pointer" }}
+                  ></i>
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
           </Form>
         </Navbar.Collapse>
       </Container>
